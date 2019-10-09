61cc9dd8db93142de33e7b1307f0fb864663b779
Sanz <santinoo@emea.roche.com>
2019-10-09 14:47:59 +0200

    layout draft

--- front/src/layout/app.layout.tsx
+++ front/src/layout/app.layout.tsx
@@ -1,6 +1,22 @@
 import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
-// TODO (next examples): move style to CSS in JS
-export const AppLayout: React.FunctionComponent = props => (
-  <div>{props.children}</div>
-);
+export const AppLayout: React.FunctionComponent = props => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Hello World
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {props.children}
+    </div>
+  );
+};
