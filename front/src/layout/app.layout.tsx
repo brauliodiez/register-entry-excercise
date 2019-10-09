4aa6eb084a58ec2bcd69ab74bde2c55c1709024f
martir71 <ruben.martinez-lopez@roche.com>
2019-10-09 16:20:09 +0200

    Header graham

--- front/src/layout/app.layout.tsx
+++ front/src/layout/app.layout.tsx
@@ -4,18 +4,45 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 export const AppLayout: React.FunctionComponent = props => {
+  const classes = useStyles(props);
+
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit">
-            Hello World
+          <img
+            src="https://as2.ftcdn.net/jpg/01/91/20/55/500_F_191205513_1umMSGC0e2xJNO4ufUHYhwvOKEuiK5zC.jpg"
+            alt="Flying free"
+            height="40"
+            width="40"
+          />
+          <Typography
+            variant="h6"
+            color="inherit"
+            className={classes.title}
+            align="left"
+          >
+            &nbsp;&nbsp;Register enter and exit
+          </Typography>
+          <Typography variant="h6" color="inherit" align="right">
+            &nbsp;&nbsp;Register 2
           </Typography>
         </Toolbar>
       </AppBar>
-
       {props.children}
     </div>
   );
