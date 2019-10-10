9f9eef18d2662bce33da54d8ee31a45a949f1a18
mariaspj <jordi.marias_parella@roche.com>
2019-10-10 10:00:38 +0200

    Tratando de maquetar

--- front/src/pods/register-entry/register-entry.component.tsx
+++ front/src/pods/register-entry/register-entry.component.tsx
@@ -6,25 +6,32 @@ import TextField from "@material-ui/core/TextField";
 import { registerEntryViewModel } from "./register-entry.vm";
 import Container from "@material-ui/core/Container";
 import SignatureCanvas from "react-signature-canvas";
-import Box from "@material-ui/core/Box";
+import { element } from "prop-types";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       display: "flex",
-      flexWrap: "wrap",
       justifyContent: "center"
     },
     textField: {
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
-      width: 200
+      width: 200,
     },
     dense: {
       marginTop: 19
     },
-    menu: {
-      width: 200
+    elementWrapper: {
+      flexDirection: "column"
+    },
+    highElementWrapper: {
+      flexDirection: "row"
+    },
+    element: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 100
     }
   })
 );
@@ -40,69 +47,63 @@ export const RegisterEntryComponent = (props: Props) => {
   const { onChange, registerEntry } = props;
 
   return (
-    <Box color="rgba(240,240,240,255)">
-      <form className={classes.container} noValidate autoComplete="off">
-        <Container fixed>
-          <div>
-            <TextField
-              id="standard-name"
-              label="Card Number"
-              className={classes.textField}
-              value={registerEntry.cardNumber}
-              onChange={onChange}
-              margin="normal"
-            />
-            <TextField
-              id="date"
-              label="Birthday"
-              type="date"
-              defaultValue="2017-05-24"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-            <TextField
-              id="time"
-              label="Alarm clock"
-              type="time"
-              defaultValue="07:30"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true
-              }}
-              inputProps={{
-                step: 300 // 5 min
-              }}
-            />
+    <form noValidate autoComplete="off">
+      <div className={classes.highElementWrapper}>
+        <div className={classes.elementWrapper}>
+          <TextField
+            id="standard-name"
+            label="Card Number"
+            className={classes.textField}
+            value={registerEntry.cardNumber}
+            onChange={onChange}
+            margin="normal"
+          />
+          <TextField
+            id="standard-name"
+            label="Name"
+            className={classes.textField}
+            value={registerEntry.firstName}
+            onChange={onChange}
+            margin="normal"
+          />
+
+          <TextField
+            id="standard-name"
+            label="DNI"
+            className={classes.textField}
+            value={registerEntry.firstName}
+            onChange={onChange}
+            margin="normal"
+          />
+          <TextField
+            id="standard-name"
+            label="Visits"
+            className={classes.textField}
+            value={registerEntry.lastName}
+            onChange={onChange}
+            margin="normal"
+          />
+        </div>
+        <div className={classes.elementWrapper}>
+          <div className={classes.highElementWrapper}>
+            <div>
+              <label>Date:</label>
+              {registerEntry.date}
+            </div>
+            <div>
+              <label>Hour:</label>
+              {registerEntry.hour}
+            </div>
           </div>
+          <TextField
+            id="standard-name"
+            label="Last Name"
+            className={classes.textField}
+            value={registerEntry.lastName}
+            onChange={onChange}
+            margin="normal"
+          />
           <div>
-            <TextField
-              id="standard-name"
-              label="Name"
-              className={classes.textField}
-              value={registerEntry.firstName}
-              onChange={onChange}
-              margin="normal"
-            />
-            <TextField
-              id="standard-name"
-              label="Last Name"
-              className={classes.textField}
-              value={registerEntry.lastName}
-              onChange={onChange}
-              margin="normal"
-            />
-          </div>
-          <div>
-            <TextField
-              id="standard-name"
-              label="DNI"
-              className={classes.textField}
-              value={registerEntry.firstName}
-              onChange={onChange}
-              margin="normal"
-            />
             <TextField
               id="standard-name"
               label="Company"
@@ -112,23 +113,13 @@ export const RegisterEntryComponent = (props: Props) => {
               margin="normal"
             />
           </div>
-          <div>
-            <TextField
-              id="standard-name"
-              label="Visits"
-              className={classes.textField}
-              value={registerEntry.lastName}
-              onChange={onChange}
-              margin="normal"
-            />
-          </div>
-          <SignatureCanvas
-            penColor="black"
-            backgroundColor="rgba(240,240,240,255)"
-            canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
-          />
-        </Container>
-      </form>
-    </Box>
+        </div>
+      </div>
+      <SignatureCanvas
+        penColor="black"
+        backgroundColor="rgba(240,240,240,255)"
+        canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+      />
+    </form>
   );
 };
